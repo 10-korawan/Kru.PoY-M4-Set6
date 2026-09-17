f4d9bc02cbd419c59cefccf96641786d8c0e8890
10-korawan <korawan020853@gmail.com>
2026-09-17 16:48:52 +0700

    Update Examination_2.py

--- Examination_2.py
+++ Examination_2.py
@@ -6,4 +6,8 @@
 
 
 # นักเรียนเขียนโค้ดต่อจากบรรทัดนี้
-height = int(input())
+height = int(input("ตรวจสอบความสูงสำหรับเครื่องเล่น: "))
+if height >=140:
+  print("can Ride")
+else:
+  print("cannot Ride")
